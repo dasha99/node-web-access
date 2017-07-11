ff0d16f50715c02a4fe6702dcf1f97bacdd8dc7e
dasha99 <abhishek.dash@gmail.com>
2017-07-10 18:42:29 -0700

    Changing port on Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.PORT || 3000;
+const port = process.PORT || 8001;
 
 var app = express();
 
