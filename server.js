4ec03567837c75b19532de25e1cb5dcdd511390d
dasha99 <abhishek.dash@gmail.com>
2017-07-10 23:51:31 -0700

    Chaning the port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.PORT || 6000;
+const port = process.PORT || 3000;
 
 var app = express();
 
